3f9481cba586e699c3596fa1beae60718a43c30b
si-saaref <achmadsyarif241@gmail.com>
2023-07-08 11:32:34 +0700

    feat: add link note

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,5 +1,5 @@
 docker build -t item-app:v1 .
 docker image ls
 docker image tag item-app:v1 sisaaref/item-app:v1
-docker login
+echo $PASSWORD_DOCKER_HUB | docker login -u sisaaref --password-stdin
 docker push sisaaref/item-app:v1
