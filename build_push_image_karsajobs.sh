a7bce4b7268beae3b4ffc7ff23c936bcc9f571e7
si-saaref <achmadsyarif241@gmail.com>
2023-07-10 13:59:01 +0700

    feat: create script to build and push image to docker hub

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,11 @@
+# Todo => creating docker iamge
+docker build -t karsajobs:latest
+
+# Todo => creating new image that refers to docker hub image repo
+docker tag karsajobs:latest sisaaref/karsajobs:latest
+
+# Todo => login to docker local
+echo $PASSWORD_DOCKER_HUB | docker login -u sisaaref --password-stdin
+
+# Todo => pushing image on local to repo docker hub
+docker push sisaaref/karsajobs:latest
